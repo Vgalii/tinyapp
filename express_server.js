ef0f6b08408edeadbf8143ae6f31d16ea27f3313
Vgalii <veronicaggalii@gmail.com>
2021-12-08 13:04:17 -0500

    Install cookie parser and added login functionality to the header

--- express_server.js
+++ express_server.js
@@ -3,8 +3,9 @@ const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(cookieParser());
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -13,12 +14,20 @@ const urlDatabase = {
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
+app.post("/login", (req, res) => {
+  
+  res.cookie("username", req.body["username"]);
+  res.redirect("/urls");
+  console.log(username)
+})
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {username: null}
+  res.render("urls_new", templateVars);
 });
 app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
@@ -29,7 +38,7 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
