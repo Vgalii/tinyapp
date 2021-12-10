9bbbe554f2952a2c41f0680fde77cbf3eeb31708
Vgalii <veronicaggalii@gmail.com>
2021-12-10 16:34:20 -0500

    Fixed some bugs

--- express_server.js
+++ express_server.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; 
+const PORT = 8080;
 
-const cookieSession = require('cookie-session')
+const cookieSession = require('cookie-session');
 const bcrypt = require("bcryptjs");
 const bodyParser = require("body-parser");
 
-app.use (cookieSession({
+app.use(cookieSession({
   name: 'session',
   keys: ["key1", "key2"],
-}))
+}));
 
 app.set("view engine", "ejs");
 
@@ -22,23 +22,23 @@ const urlDatabase = {
   "b2xVn2": {longURL: "http://www.lighthouselabs.ca", user_id:"aJ48lW"},
   "9sm5xK": {longURL: "http://www.google.com", user_id:"3ik5g3"}
 };
-const users = { 
+const users = {
   "3ik5g3": {
-    id: "3ik5g3", 
-    email: "bob@gmail.com", 
+    id: "3ik5g3",
+    email: "bob@gmail.com",
     password: "bobbob"
   },
- "34rt34": {
-    id: "34rt34", 
-    email: "peter@gmail.com", 
+  "34rt34": {
+    id: "34rt34",
+    email: "peter@gmail.com",
     password: "peterpeter"
   }
-}
+};
 
 ///Helper functions
 
 const { getUserByEmail } = require('./helpers');
-function generateRandomString () {
+function generateRandomString() {
   const result = Math.random().toString(36).substring(2,8);
   return result;
 }
@@ -47,36 +47,36 @@ const checkByCookie = function(req) {
   const user_id = req.session.user_id;
   let userObject = users[user_id];
   return userObject;
-}
+};
 const checkURLByUser = function(user_id, urlDatabase) {
   let urlObject = {};
   for (let shortURL in urlDatabase) {
-    if ( urlDatabase[shortURL].user_id === user_id) {
+    if (urlDatabase[shortURL].user_id === user_id) {
       urlObject[shortURL] = urlDatabase[shortURL];
     }
   }
   return urlObject;
-}
+};
 //ROUTS
 app.get("/", (req, res) => {
-  
+  const user_id = req.session.user_id;
   const user = checkByCookie(req);
   if (user) {
     const userURLs = checkURLByUser(user_id, urlDatabase);
     const templateVars = {userURLs, user};
     res.render("urls_index", templateVars);
   }
-  res.redirect(400, "/login")
+  res.redirect(400, "/login");
 });
 app.post("/", (req, res) => {
   
   const user_id = req.session.user_id;
   const longURL = req.body.longURL;
   const shortURL = generateRandomString();
-  urlDatabase[shortURL] = {longURL, user_id}
+  urlDatabase[shortURL] = {longURL, user_id};
 
-  res.redirect(`urls/${shortURL}`)
-})
+  res.redirect(`urls/${shortURL}`);
+});
 
 // login
 app.post("/login", (req, res) => {
@@ -98,9 +98,9 @@ app.post("/login", (req, res) => {
     }
   } else {
     res.redirect(403, '/login');
-  } 
+  }
 
-})
+});
 
 app.get("/login", (req, res) => {
   
@@ -108,22 +108,22 @@ app.get("/login", (req, res) => {
   const user_id = req.session.user_id;
   const templateVars = {"user_id": user_id, user: users[user_id]};
   res.render("login", templateVars);
-})
+});
 // logout
 app.post("/logout", (req, res) => {
   
   req.session = null;
   res.redirect("/urls");
-})
+});
 
 // register
 app.get("/register", (req, res) => {
   
   const user_id = req.session.user_id;
   let templateVars = {"user_id": user_id, user: users[user_id]};
-  res.render("register", templateVars)
+  res.render("register", templateVars);
  
-})
+});
 app.post("/register", (req, res) => {
   const id = generateRandomString();
   const email = req.body.email;
@@ -142,7 +142,7 @@ app.post("/register", (req, res) => {
     res.redirect('/urls');
     console.log(users[id]);
   }
-})
+});
 // URLs
 app.get("/urls", (req, res) => {
   
@@ -164,7 +164,7 @@ app.get("/urls/new", (req, res) => {
     res.render("urls_new", templateVars);
     return;
   }
-  res.redirect(403, "/login")
+  res.redirect(403, "/login");
   
 });
 app.post("/urls", (req, res) => {
@@ -176,26 +176,37 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 app.get("/urls/:shortURL", (req, res) => {
-
+  const shortURL = req.params.shortURL;
   const user_id = req.session.user_id;
   const user = checkByCookie(req);
   const userURLs = checkURLByUser(user_id, urlDatabase);
 
-  if(user) {
-    const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: users[user]};
-    res.render("urls_show", templateVars);
-    return;
+  if (user) {
+    if (urlDatabase[shortURL]) {
+      if (userURLs[shortURL]) {
+        const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: users[user]};
+        res.render("urls_show", templateVars);
+        return;
+      } else {
+        res.redirect(403, "/login");
+        return;
+      }
+    } else {
+      res.redirect(403, "/login");
+      return;
+    }
   }
-  res.redirect(403, "/login")
+  res.redirect(403, "/login");
+  return;
 });
 app.post("/urls/:shortURL", (req, res) => {
   
   const user_id = req.session.user_id;
   const shortURL = req.params.shortURL;
   const longURL = req.body.longURL;
-  urlDatabase[shortURL] = {longURL, user_id}
-  res.redirect("/urls")
-})
+  urlDatabase[shortURL] = {longURL, user_id};
+  res.redirect("/urls");
+});
 app.get("/urls.json", (req, res) => {
   res.json(users);
 });
@@ -209,10 +220,10 @@ app.post("/urls/:shortURL/edit", (req, res) => {
   const userURLs = checkURLByUser(user_id, urlDatabase);
 
   if (user) {
-    if (userURLs[shortURL]){
-    urlDatabase[shortURL].longURL = longURL;
-    res.redirect("/urls/");
-    return;
+    if (userURLs[shortURL]) {
+      urlDatabase[shortURL].longURL = longURL;
+      res.redirect("/urls/");
+      return;
     } else {
       res.redirect(403, "/login");
       return;
@@ -223,7 +234,7 @@ app.post("/urls/:shortURL/edit", (req, res) => {
     return;
   }
   
-})
+});
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(`http://${longURL["longURL"]}`);
@@ -236,7 +247,7 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   const userURLs = checkURLByUser(user_id, urlDatabase);
   if (user) {
     
-    if (userURLs[shortURL]){
+    if (userURLs[shortURL]) {
       delete urlDatabase[shortURL];
       res.redirect("/urls");
       return;
@@ -248,9 +259,9 @@ app.post("/urls/:shortURL/delete", (req, res) => {
     res.redirect(403, "/login");
     return;
   }
-})
+});
 
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
-})
+});
